a8560fcc2b310a757db19bf0b63145788fa7fcb6
Shrutinkrishnsamy <shrutinsuper@gmail.com>
2026-07-08 13:53:10 +0530

    Initial commit

---
+++ src/main/java/com/ad/Adminservlet.java
@@ -0,0 +1,35 @@
+package com.ad;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Adminservlet
+ */
+@WebServlet("/Adminservlet")
+public class Adminservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		
+		out.print("<html><body>");
+		out.print("<h1>welcome to admin page </h1>");
+		out.print("</body></html>");
+		
+		
+	}
+
+}
